150fc7339c1a3a6a7d29e23b34a376c6135b59cf
Loic Rosset <loicrossetmp@gmail.com>
2023-06-01 11:52:21 +0200

    feat: Ajout des layers control

--- components/Map/map.js
+++ components/Map/map.js
@@ -1,6 +1,6 @@
 "use client";
 import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer, Marker, Popup, Circle, LayerGroup } from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup, Circle, LayerGroup, LayersControl } from "react-leaflet"
 
 import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
@@ -10,15 +10,6 @@ function getIcon() {
         iconUrl: icon.src,
         iconSize: [25, 40],
     });
-}
-
-function setSwitzerlandMap() {
-    var swissLayer = L.tileLayer.swiss(/* options */);
-    return swissLayer;
-}
-
-function getLayerGroup() {
-    return L.layerGroup();
 }
 
 function Map({offers}){
@@ -39,14 +30,44 @@ function Map({offers}){
     }
     return (
         <MapContainer center={[46.519962, 6.633597]} zoom={13} scrollWheelZoom={true} style={{ height: "100vh", width: "100%" }}>
-            <LayerGroup
-                attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+            <LayersControl position="topright">
+                <LayersControl.BaseLayer checked name="OpenStreetMap Mapnik">
+                    <TileLayer 
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer name="OpenStreetMap.DE">
+                    <TileLayer 
+                        url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer name="OpenStreetMap.France">
+                    <TileLayer 
+                        url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
+                        attribution='&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer name="OpenStreetMap.CH">
+                    <TileLayer 
+                        url="https://tile.osm.ch/switzerland/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer name="Dark">
+                    <TileLayer 
+                        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" 
+                        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                    />
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer name="Ragnarok">
+                    <TileLayer 
+                        url='https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png?apikey=9333d9105aa443ac9500e5ef07137cc4'
+                        attribution='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </LayersControl.BaseLayer>
+            </LayersControl>
             <Circle center={[46.519962, 6.633597]} pathOptions={{ color: 'red' }} radius={2000} />
             {rows}
         </MapContainer>
